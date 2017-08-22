7004a3801c9ca6f4461b90cef023c48639033049
RyeBukowski <josephbtran@gmail.com>
2017-08-22 05:13:58 -0700

    Test Run

---
+++ key.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'oNyWgqsRb9AOcWKjObCdtbSRc',
+  consumer_secret: 'YW0XzCDtqA6HYCSi2X2MjJTPuadvNWTJ39qhSMp82oY0412iJt',
+  access_token_key: '899710532193615873-JwIkOeNvlVOcdzjVaDuxZfxUbBRsBnA',
+  access_token_secret: 'hJoa0WS6GMtfzpo6Dy2uLWvpViS9etJKTX3X3vrtuiH3A',
+}
+
